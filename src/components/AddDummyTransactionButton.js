0d783dbcd77b9774c8fd0e572070f462d0143612
Roberto Fierros <roberto@wizeline.com>
2018-02-20 17:49:11 -0600

    Add dummy transaction

--- src/components/AddDummyTransactionButton.js
+++ src/components/AddDummyTransactionButton.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
 const AddDummyTransactionButton = () => {
+
+  const handleClick = () => {
+    const newTransaction = {
+      id: `${Math.random()}`,
+      crypto: 'eth',
+      type: 'buy',
+      price: 850,
+      amount: 10.2367,
+    };
+  };
+
   return (
-    <button onClick={() => {}}>Add dummy transaction</button>
+    <button onClick={handleClick}>Add dummy transaction</button>
   );
 };
 
