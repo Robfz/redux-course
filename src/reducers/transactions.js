8878284b9b22c811af94b8bec7c0d04405b62436
Roberto Fierros <roberto@wizeline.com>
2018-02-20 18:00:28 -0600

    Scrap transactions reducer so the students can reimplement it

--- src/reducers/transactions.js
+++ src/reducers/transactions.js
@@ -1,30 +1,7 @@
 import transactionsActions from '../actions/transactions';
 
-const initialState = {};
-
-const transactionsReducer = (state = initialState, action) => {
-  const { type, payload } = action;
-
-  switch (type) {
-    case transactionsActions.types.TRANSACTION_ADD: {
-      const { transaction } = payload;
-
-      const newState = { ...state };
-      newState[transaction.id] = { ...transaction };
-
-      return newState;
-    }
-    case transactionsActions.types.TRANSACTION_REMOVE: {
-      const { transaction } = payload;
-
-      const newState = { ...state };
-      delete newState[transaction.id];
-
-      return newState;
-    }
-    default:
-      return state;
-  }
+const transactionsReducer = () => {
+  return {};
 };
 
 export default transactionsReducer;
